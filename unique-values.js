38c9a27343b0a3ed4b812bbfa809fd22649e3f26
Rob <robrichardsdpt@gmail.com>
2021-02-03 12:56:54 -0500

    unique values completed O(n) time and O(n) space

---
+++ unique-values.js
@@ -0,0 +1,11 @@
+function countUniqueValues(arr){
+  const arrObj = {}
+  for(let i = 0; i < arr.length; i++) {
+      !arrObj[arr[i]] ? arrObj[arr[i]] = 1 : arrObj[arr[i]]++
+  }
+  let count = 0
+  for (let key in arrObj) {
+      count++
+  }
+  return count
+}
