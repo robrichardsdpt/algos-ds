1cc0f671da36fd705eebd2e0a0d4c1bc893ea96b
Rob <robrichardsdpt@gmail.com>
2021-02-03 17:07:28 -0500

    added substring search.js

---
+++ search-algos/substring-search.js
@@ -0,0 +1,14 @@
+function substringInString (str, sub) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j<sub.length; j++) {
+      if(str[i+j] === sub[j]) {
+        break
+      }
+      if(j=== sub.length -1) {
+        count++
+      }
+    }
+  }
+  return count
+}
