34273daadd8d7c11c52f842b1d3fba8b43887246
Rob <robrichardsdpt@gmail.com>
2021-02-03 11:26:38 -0500

    first commit with check-anagram completed.

---
+++ check-anagram.js
@@ -0,0 +1,30 @@
+function validAnagram(str1, str2){
+  const freqInStr1 = {}
+  if(str1.length !== str2.length) {
+    return false
+  }
+  for(let char of str1) {
+     if(!freqInStr1[char]) {
+         freqInStr1[char] = 1
+     } else {
+         freqInStr1[char]++
+     }
+  }
+  const freqInStr2 = {}
+  for(let char of str2) {
+     if(!freqInStr2[char]) {
+         freqInStr2[char] = 1
+     } else {
+         freqInStr2[char]++
+     }
+  }
+  for(let key in freqInStr1) {
+      if(freqInStr1[key] !== freqInStr2[key]) {
+          return false
+      }
+      if(!freqInStr2[key]) {
+          return false
+      }
+  }
+  return true
+}
---
+++ reverse-string.js
@@ -0,0 +1,5 @@
+const reverseString = (str) => {
+  return str.reverse()
+}
+
+reverseString('Hello')
