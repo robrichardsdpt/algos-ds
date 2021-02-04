1dbac63470ae43c1f27306c753a23c07ba5cd76a
Rob <robrichardsdpt@gmail.com>
2021-02-04 09:37:27 -0500

    added selection-sort.js

---
+++ sorting-algos/selection-sort.js
@@ -0,0 +1,16 @@
+const selectionSort = (arr) => {
+  for(let i = 0; i < arr.length; i++) {
+    let min = i
+    for(let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j
+      }
+    }
+    if(i !== lowest){
+      [arr[i], arr[min]] = [arr[min], arr[i]]
+    }
+  }
+  return arr
+}
+
+selectionSort([9,65,2,4,8,43,1,3,7,205])
