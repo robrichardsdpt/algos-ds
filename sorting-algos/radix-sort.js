fb9937b9af360dbb40fbe69c3f7bc95631fc44d1
Rob <robrichardsdpt@gmail.com>
2021-02-04 11:35:35 -0500

    added radix-sort for  positive numbers

---
+++ sorting-algos/radix-sort.js
@@ -0,0 +1,30 @@
+// only working for positive numbers
+
+const radixSort = (arr) => {
+  let maxDigitCount = mostDigits(arr)
+  for(let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({length: 10}, () => [])
+    for (let i =0; i < arr.length; i++) {
+      digitBuckets[getDigit(arr[i], k)].push(arr[i])
+    }
+    arr = [].concat(...digitBuckets)
+  }
+  return arr
+}
+
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num)/ Math.pow(10, i)) % 10
+}
+
+const digitCount = (num) => {
+  if(num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+const mostDigits = (nums) => {
+  let maxDigits = 0 
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+  }
+  return maxDigits
+}
