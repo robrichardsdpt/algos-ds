f0bf503726842a43fa4d8832ef886c0b66ec3770
Rob <robrichardsdpt@gmail.com>
2021-02-08 10:55:54 -0500

    added BFS

---
+++ data-structures/binary-tree.js
@@ -0,0 +1,76 @@
+class BinaryTree {
+  constructor() {
+    this.root = null
+  }
+  insert (value) {
+    let newNode = new Node(value)
+    if (!this.root) {
+      this.root = newNode
+      return this
+    } else {
+      let current = this.root
+      while(true){
+        if(value === current.value) return undefined
+        if(value < current.value){
+          if(current.left === null) {
+            current.left = newNode
+            return this
+          } else {
+            current = current.left
+          }
+        } else if(value > current.value){
+          if(current.right === null) {
+            current.right = newNode
+            return this
+          } else {
+            current = current.right
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if(this.root === null) return false
+    let current = this.root
+    let found = false
+    while(!found && current) {
+      if(value < current.value) {
+        current = current.left
+      } else if (value > current.value) {
+        current = current.right
+      } else {
+        found = true
+      }
+    }
+    if(!found) return false
+    return current
+  }
+  BFS () {
+    let data = []
+    let queue = []
+    let node = this.root
+    queue.push(this.root)
+    while(queue.length){
+      node = queue.shift()
+      data.push(node)
+      if(node.left) queue.push(node.left)
+      if(node.right) queue.push(node.right)
+    }
+    return data
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.right = null
+    this.left = null
+  }
+}
+
+let tree = new BinaryTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(14)
+tree.insert(7)
+tree.insert(2)
